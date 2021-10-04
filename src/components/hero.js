9ab525ffdb5451134a11feb2f9b6ba291628af19
nthhm <numasawa3228@gmail.com>
2021-10-04 20:24:36 +0900

    first commit

---
+++ src/components/hero.js
@@ -0,0 +1,27 @@
+import React from "react"
+import "../styles/hero.css"
+
+export default function Hero() {
+  return (
+    <div className="hero">
+      <h1 className="hero-text">
+        Welcome to my blog!
+        <br />
+        Enjoy lots of posts.
+        <br />
+      </h1>
+      <div className="hero-author">
+        <img
+          src="https://source.unsplash.com/IuJc2qh2TcA"
+          alt="avatar"
+          className="hero-author-image"
+        ></img>
+        <p>
+          Written by Takahiro.
+          <br />
+          Front Engineer at Hoge.Inc. Love JavaScript.
+        </p>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/layout.js
@@ -0,0 +1,16 @@
+import React from "react"
+import "../styles/layout.css"
+import Footer from "./footer"
+import Header from "./header"
+
+const Layout = ({ children }) => {
+  return (
+    <div className="layout">
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
